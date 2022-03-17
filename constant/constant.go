a96820dce54bf0cc0a8ca456e380629eaf9abb12
Viram Jain <viramjain9742@gmail.com>
2022-03-17 12:13:35 +0530

    changed path of env and logger

--- constant/constant.go
+++ constant/constant.go
@@ -1,6 +1,6 @@
 package constant
 
 const (
-	Envfile = "/home/AD.KALEYRA.LOCAL/viram.jain/go/src/rmqandredis/.env"
-	Logfile = "/home/AD.KALEYRA.LOCAL/viram.jain/go/src/rmqandredis/logfile.log"
+	Envfile = ".env"
+	Logfile = "logfile.log"
 )
